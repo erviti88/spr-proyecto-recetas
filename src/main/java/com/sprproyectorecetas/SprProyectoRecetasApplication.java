37d38e061f48547468d113f7b9654882bfcbe978
erviti88 <ignacio.erviti@bootcamp-upgrade.com>
2024-02-09 02:44:11 +0100

    Proyecto inicializado

---
+++ src/main/java/com/sprproyectorecetas/SprProyectoRecetasApplication.java
@@ -0,0 +1,13 @@
+package com.sprproyectorecetas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprProyectoRecetasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SprProyectoRecetasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sprproyectorecetas/SprProyectoRecetasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sprproyectorecetas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SprProyectoRecetasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
